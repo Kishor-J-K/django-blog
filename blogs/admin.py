7cdee166bb75ccf88927706d4f916597ab458310
Kishor-J-K <Kishor-J-K@users.noreply.github.com>
2026-08-19 22:31:34 +0530

    Blog model initiated

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import category
+from .models import Category
 
-admin.site.register(category)
+admin.site.register(Category)
--- blogs/models.py
+++ blogs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class category(models.Model):
+class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -12,3 +13,28 @@ class category(models.Model):
     
     def __str__(self):
         return self.category_name
+
+STATUS_CHOICES = (
+    (0, 'Draft'),
+    (1, 'Published'),
+)
+    
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, unique=True,blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d/', blank=True)
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=5000)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+    
+    def __str__(self):
+        return self.title
