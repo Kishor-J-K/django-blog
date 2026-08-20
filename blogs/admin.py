cb8674c46e31185c7ac23166c5715cbd8b1b0c66
Kishor-J-K <Kishor-J-K@users.noreply.github.com>
2026-08-20 15:53:31 +0530

    Blog model initiated

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category,Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author', 'is_featured')
+    search_fields = ('title', 'category__category_name', 'author__username')
+    list_editable = ('is_featured',)
 
 admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
